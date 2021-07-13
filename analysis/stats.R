0bff58882489df3089b5210f8dee0e70237282fc
Nicholas Medina <nmedina@umich.edu>
2021-07-13 01:01:59 -0400

    added entropy calc, need to mutate it before nesting and stat pipeline

--- analysis/stats.R
+++ analysis/stats.R
@@ -4,6 +4,12 @@ library(tidyverse); library(rstatix)
 plotVarsTbl <- rawData %>%
   group_by(dist, plot) %>%
   mutate(richness = n_distinct(sp)) %>%
+  add_count(gen, name = "n_gen") %>%
+  pivot_wider(names_from = gen, values_from = n_gen,
+              values_fill = 0, values_fn = sum,
+              names_repair = "universal") %>%
+  select(-(ID : richness)) %>%
+  diversity() #mutate
   select(luz, incl, #abiotic
          dap, h, spg, kg17, #biotic
          richness) %>% #taxonomic
@@ -15,7 +21,6 @@ treeVarsTbl <- rawData %>%
   select(dap, h, spg, kg17) %>%
   get_summary_stats(type = "median_mad") %>%
   mutate(median1 = log10(median + 1)) %>% #4statTests
-  #mutate(spTbl = rawData %>% pivot_wider(gen, median)) %>%
   nest(varData = -variable)
 
 #funcw/formulahard
