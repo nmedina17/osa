28b08a72744a585bc796f00b356e7da719bd74d7
Nicholas Medina <nmedina@umich.edu>
2021-10-21 10:52:13 -0400

    do clean, stats, fig3 for dispersal data

---
+++ figs/fig3.R
@@ -0,0 +1,38 @@
+library(
+  here
+)
+i_am(
+  "analysis/figs/fig3"
+)
+source(
+  here(
+    "analysis/stats.R"
+  )
+)
+source(
+  here(
+    "figs/style.R"
+  )
+)
+source(
+  here(
+    "figs/fig1.R"
+  )
+)
+
+
+#plan:
+#dist-x-Xkernels4dispersalModes
+
+varForm
+xAxisLabel <- quote(
+  "Distance to secondary forest edge (m)"
+)
+statData
+underData
+
+
+
+#panel
+
+graph1
