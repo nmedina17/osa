0d8d9f40b6be4e3d28ca7217d999919737d749e9
nmedina17 <nmedina@umich.edu>
2022-08-04 19:02:53 -0400

    rev meth,intro MES need disc,abs

--- figs/map.R
+++ figs/map.R
@@ -1,7 +1,7 @@
 here::i_am("figs/map.R")
 
-site <- magick::image_read(here::here("figs/map.png")) %>% magick::image_ggplot()
-site0 <- magick::image_read(here::here("figs/locationWOW.jpg")) %>% magick::image_ggplot()
+site <- magick::image_read(here::here("figs/mapMod.png")) %>% magick::image_ggplot()
+site0 <- magick::image_read(here::here("figs/locationWOWmod.jpg")) %>% magick::image_ggplot()
 
 Osa <- magick::image_read(here::here("figs/map_Osa.jpg")) %>% magick::image_ggplot()
 pochote <- magick::image_read(here::here("figs/pochote.jpg")) %>% magick::image_ggplot()
